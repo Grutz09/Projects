89b4f0a2d5de950c65f8cf9a8c92c10d8e78afb2
Grutz09 <judemelvingrutas@gmail.com>
2025-10-05 22:08:18 +0800

    done

--- Practice/ppp.py
+++ Practice/ppp.py
@@ -8,6 +8,6 @@ import pandas as pd
 # series = pd.Series([25000,23254,21200,26880], index=['2020 Revenue', '2021 Revenue', '2022 Revenue', '2023 Revenue'], name='Yearly Revenues')
 # print(series)
 
-wine_review = pd.read_csv(r"C:\Users\seanandrew\Desktop\kaggle_datasets\winemag-data-130k-v2.csv")
+wine_review = pd.read_csv(r"C:\Users\seanandrew\Desktop\kaggle_datasets\winemag-data-130k-v2.csv", index_col=0)
 
-print(wine_review.country)
+print(wine_review.iloc[[0, 1, 2], 10])   
