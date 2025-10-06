12f786229ca83f28a928e830775034a5ff90e933
Grutz09 <judemelvingrutas@gmail.com>
2025-10-06 21:54:04 +0800

    practice

--- Practice/ppp.py
+++ Practice/ppp.py
@@ -1,13 +1,13 @@
+import streamlit as st
 import pandas as pd
 
-# data = pd.DataFrame({'Samsung': ['Galaxy z21', 'A7 Note'], 
-#                      'Oppo': ['A5', 'A16'] 
-#                      }, index=['2020', '2021'])
-# print(data)
+st.write("""
+         # My First App
+         # Hello *world*
+         """
+         )
 
-# series = pd.Series([25000,23254,21200,26880], index=['2020 Revenue', '2021 Revenue', '2022 Revenue', '2023 Revenue'], name='Yearly Revenues')
-# print(series)
+wine = pd.read_csv(r"C:\Users\seanandrew\Desktop\kaggle_datasets\winemag-data-130k-v2.csv", index_col=0)
+# winer = wine.loc([(wine.country('Italy')) & (wine.points>90)])
 
-wine_review = pd.read_csv(r"C:\Users\seanandrew\Desktop\kaggle_datasets\winemag-data-130k-v2.csv", index_col=0)
-
-print(wine_review.loc[wine_review.country == 'US'])   
+st.line_chart(wine.country)
