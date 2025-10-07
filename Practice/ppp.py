63bb1f7f66ce732e6cfef7a121964d889ef00ceb
Grutz09 <judemelvingrutas@gmail.com>
2025-10-07 22:13:58 +0800

    prac

--- Practice/ppp.py
+++ Practice/ppp.py
@@ -8,7 +8,13 @@ wine = pd.read_csv(r"C:\Users\seanandrew\Desktop\kaggle_datasets\winemag-data-13
 # bargain_wine = wine.loc[ratio, 'title']
 
 
-tropical = wine['description'].map(lambda p: 'tropical' in p).sum()
-fruity = wine['description'].map(lambda p: 'fruity' in p).sum()
-descriptor_counts = pd.Series([tropical, fruity], index = ['tropical', 'fruity'])
-print(descriptor_counts)
+# tropical = wine['description'].map(lambda p: 'tropical' in p).sum()
+# fruity = wine['description'].map(lambda p: 'fruity' in p).sum()
+# descriptor_counts = pd.Series([tropical, fruity], index = ['tropical', 'fruity'])
+
+three_star = wine['points'].loc[(wine.points >= 95) | (wine.country == 'Canada')].map(wine)
+two_star = wine['points'].loc[wine.points >=85].map(wine)
+one_star = wine['points'].loc[wine.points < 85].map(wine)
+
+star_rating = pd.Series([three_star, two_star, one_star], index=['3 Star', '2 Star', '1 Star'])
+print(star_rating[:10])
