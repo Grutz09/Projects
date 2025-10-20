56bc6fda4927a89d4c317cd38d2e36d9cc3a82ae
Grutz09 <judemelvingrutas@gmail.com>
2025-10-20 14:56:49 +0800

    prac

--- Practice/cleaning.py
+++ Practice/cleaning.py
@@ -1,5 +1,21 @@
 import pandas as pd
 
 df = pd.read_excel(r"C:\Users\seanandrew\Desktop\datasets\Customer Call List.xlsx")
-df_rmdupli = df.drop_duplicates()
-print(df_rmdupli.Last_Name)
+
+# ## NOT WORKING 
+# print(df.isnull().sum())
+# df['Last_Name'] = df['Last_Name'].astype('str')
+# df_fill = df['Last_Name'].fillna(df.mean(), inplace=True)
+# print(df_fill)
+
+### WORKING
+# df_dupli = df.drop_duplicates()
+# df1= df_dupli.drop(columns="Not_Useful_Column")
+
+
+# df["Last_Name"] = df["Last_Name"].drop_duplicates()
+# df['Last_Name'] = df["Last_Name"].str.strip("123_/.")
+# print(df["Last_Name"])
+
+df['Phone_Number'] = df["Phone_Number"].str.replace('[^0-9]','', regex=True)
+print(df['Phone_Number'])
