e00ccbbb56156a37b6520698d2e41bd04c662a45
Grutz09 <judemelvingrutas@gmail.com>
2025-12-07 19:48:07 +0800

    done with artist score over time

--- Pitchfork Analysis/pipeline.py
+++ Pitchfork Analysis/pipeline.py
@@ -35,46 +35,46 @@ merged_df['artist'] = merged_df['artist'].str.lower().str.replace(r'[^a-z0-9\s]'
 #Day 4 EDA Charts
 
 ## Score distribution
-score_counts = merged_df.groupby('pub_year')['score'].count()
+# score_counts = merged_df.groupby('pub_year')['score'].count()
 
-plt.figure(figsize=(14, 5)) 
-plt.subplot(2,2,1)
-plt.bar(score_counts.index, score_counts.values, edgecolor='black')
-plt.xlabel('scores')
-plt.ylabel('frequency')
-plt.title('Artist Score Distribution')
+# plt.figure(figsize=(14, 5)) 
+# plt.subplot(2,2,1)
+# plt.bar(score_counts.index, score_counts.values, edgecolor='black')
+# plt.xlabel('scores')
+# plt.ylabel('frequency')
+# plt.title('Artist Score Distribution')
 
-## number of reviews per year
-required_cols = {"reviewid", "pub_date"}
-if not required_cols.issubset(merged_df.columns):
-    raise ValueError(f"Missing required columns: {required_cols - set(merged_df.columns)}")
+# ## number of reviews per year
+# required_cols = {"reviewid", "pub_date"}
+# if not required_cols.issubset(merged_df.columns):
+#     raise ValueError(f"Missing required columns: {required_cols - set(merged_df.columns)}")
 
-#convert date to datetime
-try:
-    merged_df['pub_date'] = pd.to_datetime(merged_df['pub_date'], errors="raise")
-except Exception as e:
-    raise ValueError(f"Invalid date format: {e}")
+# #convert date to datetime
+# try:
+#     merged_df['pub_date'] = pd.to_datetime(merged_df['pub_date'], errors="raise")
+# except Exception as e:
+#     raise ValueError(f"Invalid date format: {e}")
 
-#extract year
-merged_df['year'] = merged_df['pub_date'].dt.year
+# #extract year
+# merged_df['year'] = merged_df['pub_date'].dt.year
 
-#aggregate reviews per year
-reviews_per_year = merged_df.groupby('pub_year')["reviewid"].count().reset_index()
+# #aggregate reviews per year
+# reviews_per_year = merged_df.groupby('pub_year')["reviewid"].count().reset_index()
 
-#plot
-plt.subplot(2,2,2)
-plt.bar(reviews_per_year['pub_year'], reviews_per_year['reviewid'], color='lightgreen', edgecolor='black')
-plt.xlabel('Year')
-plt.ylabel('Number of Reviews')
-plt.title('Reviews Per Year')
+# #plot
+# plt.subplot(2,2,2)
+# plt.bar(reviews_per_year['pub_year'], reviews_per_year['reviewid'], color='lightgreen', edgecolor='black')
+# plt.xlabel('Year')
+# plt.ylabel('Number of Reviews')
+# plt.title('Reviews Per Year')
 
-# Average score by year
-average_scores = merged_df.groupby('year')['score'].mean()
+# # Average score by year
+# average_scores = merged_df.groupby('year')['score'].mean()
 
-plt.subplot(2,2,3)
-plt.bar(average_scores.index, average_scores.values, color='red', edgecolor='black')
-plt.xlabel('Year')
-plt.ylabel('Average Score')
+# plt.subplot(2,2,3)
+# plt.bar(average_scores.index, average_scores.values, color='red', edgecolor='black')
+# plt.xlabel('Year')
+# plt.ylabel('Average Score')
 
 #show plot
 # plt.tight_layout()
@@ -90,10 +90,21 @@ bottom_20_artist = sorted_artist.iloc[-20:]
 ## get the artist score
 filtered_artist = merged_df[merged_df['artist'] == "massive attack"]
 score_overtime = filtered_artist.groupby('pub_year')[['artist', 'score']].sum().reset_index()
+
+krallis = merged_df[merged_df['artist'] == "krallice"]
+grouped = krallis.groupby('pub_year')[['artist', 'score']].sum().reset_index()
+
+uranium = merged_df[merged_df['artist'] == "uranium club"]
+grouped1 = uranium.groupby('pub_year')[['artist', 'score']].sum().reset_index()
+# print(grouped1)
 #plot
-plt.figure(figsize=(12, 6))
-plt.bar(score_overtime['pub_year'], score_overtime['score'], edgecolor='black')
-plt.xlabel('year')
-plt.ylabel('score')
-plt.tight_layout()
-plt.show()
+# plt.figure(figsize=(12, 6))
+# plt.bar(grouped['pub_year'], grouped['score'], edgecolor='black')
+# plt.xlabel('year')
+# plt.ylabel('score')
+# plt.tight_layout()
+# plt.show()
+
+# =======================  GENRE AND REVIEW INSIGHTS ===================================
+avg_score_genre = merged_df.groupby('genre')['score'].mean()
+print(avg_score_genre)
